7d35677ab4b5c8f186b7256061e6ea4ebe8dc805
Fiser <fiser12@icloud.com>
2024-04-04 22:27:42 +0200

    PreludePackage as name

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Prelude",
+    name: "PreludePackage",
     platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Prelude",
-            targets: ["Prelude"]),
+            name: "PreludePackage",
+            targets: ["PreludePackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Prelude",
+            name: "PreludePackage",
             dependencies: []),
         .testTarget(
             name: "PreludeTests",
-            dependencies: ["Prelude"]),
+            dependencies: ["PreludePackage"]),
     ]
 )
