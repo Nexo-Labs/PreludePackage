6cbdc35cb33e42fafd69864c1c928fb473278ccf
Fiser <fiser12@icloud.com>
2024-06-21 11:27:14 +0200

    feat: Added unwrapping navigation

--- Sources/PreludePackage/Presentation/Views/SwiftUIHelpers.swift
+++ Sources/PreludePackage/Presentation/Views/SwiftUIHelpers.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIHelpers.swift
-//  
+//
 //
 //  Created by Rubén García on 14/9/23.
 //
@@ -23,7 +23,7 @@ public extension View {
             }
         })
     }
-
+    
     @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
         if condition {
             transform(self)
@@ -48,13 +48,51 @@ public extension View {
         when shouldShow: Bool,
         alignment: Alignment = .leading,
         @ViewBuilder placeholder: () -> Content) -> some View {
+            
+            ZStack(alignment: alignment) {
+                placeholder().opacity(shouldShow ? 1 : 0)
+                self
+            }
+        }
+}
 
-        ZStack(alignment: alignment) {
-            placeholder().opacity(shouldShow ? 1 : 0)
-            self
+extension View {
+    @ViewBuilder
+    public func navigationDestination<Value, Destination: View>(
+        unwrapping value: Binding<Value?>,
+        @ViewBuilder destination: (Binding<Value>) -> Destination
+    ) -> some View {
+        self.navigationDestination(isPresented: value.isPresent()) {
+            if let value = Binding(value) {
+                destination(value)
+            }
         }
     }
 }
+
+extension Binding {
+    /// Creates a binding by projecting the current optional value to a boolean describing if it's
+    /// non-`nil`.
+    ///
+    /// Writing `false` to the binding will `nil` out the base value. Writing `true` does nothing.
+    ///
+    /// - Returns: A binding to a boolean. Returns `true` if non-`nil`, otherwise `false`.
+    public func isPresent<Wrapped>() -> Binding<Bool>
+    where Value == Wrapped? {
+        self._isPresent
+    }
+}
+
+extension Optional {
+    fileprivate var _isPresent: Bool {
+        get { self != nil }
+        set {
+            guard !newValue else { return }
+            self = nil
+        }
+    }
+}
+
 
 public extension URL {
     func open() {
